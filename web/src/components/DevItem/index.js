48aa50340d409d54a3435ed8db993a302d97d17a
Jean de Oliveira Valenga <jeanvalenga@hotmail.com>
2020-01-16 17:49:52 -0300

    Finalização Mobile

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -7,16 +7,29 @@ function DevItem({dev}){
     
     return(
         <li  className="dev-item">
+           
               <header>
+                  
                 <img src={dev.avatar_url} alt={dev.name} />
+                
                 <div className="user-info">
+                    
                   <strong>{dev.name}</strong>
+                  
                   <span>{dev.techs.join(', ')}</span>
+                  
+                </div>
+                <div className="div-delete">
+                <button type="submit" className= "delete" ></button>
                 </div>
               </header>
+              
               <p>{dev.bio}</p>
+              
               <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+              
             </li>
+            
     );
 }
 
